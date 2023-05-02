e8383a067692c34e304e332847493d930918d388
Marco Acosta <marco2000corporation@hotmail.com>
2023-05-02 00:12:38 -0400

    LastUpdate02

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,43 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.slide-pro'),{
-        slidesToShow:3,
-        slidesToScroll:3,
-        dots:'.indicadores',
-        arrows:{
-            prev:'.flecha-izquierda',
-            next:'.flecha-derecha'
-        }
-    })
-})
+$(function () {
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<span class = "priv_arrow"><i class="fas fa-angle-left"></i></span>',
+        nextArrow: '<span class = "priv_arrow"><i class="fas fa-angle-right"></i></span>',
+        
+        responsive: [
+            {    
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 3,
+                    prevArrow: '<span class = "priv_arrow"><i class="fas fa-angle-left"></i></span>',
+                    nextArrow: '<span class = "priv_arrow"><i class="fas fa-angle-right"></i></span>',
+                }
+            },
+            {    
+                breakpoint: 666,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    prevArrow: '<span class = "priv_arrow"><i class="fas fa-angle-left"></i></span>',
+                    nextArrow: '<span class = "priv_arrow"><i class="fas fa-angle-right"></i></span>',
+                }
+            },
+            {
+                breakpoint: 545,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    slidesToShow: 1,
+                    prevArrow: '<span class = "priv_arrow"><i class="fas fa-angle-left"></i></span>',
+                    nextArrow: '<span class = "priv_arrow"><i class="fas fa-angle-right"></i></span>',
+               
+                }
+            }
+        ]
+    });
+});
