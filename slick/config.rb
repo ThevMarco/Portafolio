877eeded9f17eaa7b153f32fa0fc8319d1b7dbb0
Marco Acosta <marco2000corporation@hotmail.com>
2023-05-06 00:27:02 -0400

    LasUpdate-05may

--- slick/config.rb
+++ slick/config.rb
@@ -1,10 +1,1 @@
-css_dir = "."
-sass_dir = "."
-images_dir = "."
-fonts_dir = "fonts"
-relative_assets = true
-
-output_style = :compact
-line_comments = false
-
-preferred_syntax = :scss
+css_dir = "." sass_dir = "." images_dir = "." fonts_dir = "fonts" relative_assets = true output_style = :compact line_comments = false preferred_syntax = :scss
